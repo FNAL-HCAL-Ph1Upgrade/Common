9dac45eee47214edf1076ff79d5a7455940e7cf9
hcalpro <hcalpro@cmsnghcal01>
2018-06-13 16:35:18 -0500

    Now runs over full backplane

--- registerTest_run.sh
+++ registerTest_run.sh
@@ -1,4 +1,22 @@
-python registerTest.py -c 0 -r 4 -s 1
-python registerTest.py -c 0 -r 4 -s 2
-python registerTest.py -c 0 -r 4 -s 3
-python registerTest.py -c 0 -r 4 -s 4
+#!/bin/bash
+
+#RM1
+python registerTest.py -c 1 -r 1 -s 1
+python registerTest.py -c 1 -r 1 -s 2
+python registerTest.py -c 1 -r 1 -s 3
+python registerTest.py -c 1 -r 1 -s 4
+#RM2
+python registerTest.py -c 1 -r 2 -s 1
+python registerTest.py -c 1 -r 2 -s 2
+python registerTest.py -c 1 -r 2 -s 3
+python registerTest.py -c 1 -r 2 -s 4
+#RM3
+python registerTest.py -c 1 -r 3 -s 1
+python registerTest.py -c 1 -r 3 -s 2
+python registerTest.py -c 1 -r 3 -s 3
+python registerTest.py -c 1 -r 3 -s 4
+#RM4
+python registerTest.py -c 1 -r 4 -s 1
+python registerTest.py -c 1 -r 4 -s 2
+python registerTest.py -c 1 -r 4 -s 3
+python registerTest.py -c 1 -r 4 -s 4
