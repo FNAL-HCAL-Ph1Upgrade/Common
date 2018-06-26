92341a4e2420ae4610bfe426c872e116afafc9be
hcalpro <hcalpro@cmsnghcal01>
2018-06-26 11:10:21 -0500

    Adding more options

--- registerTest_run.sh
+++ registerTest_run.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 #RM1
-python registerTest.py -c 1 -r 1 -s 1
-python registerTest.py -c 1 -r 1 -s 2
-python registerTest.py -c 1 -r 1 -s 3
-python registerTest.py -c 1 -r 1 -s 4
+#python registerTest.py -c 1 -r 1 -s 1
+#python registerTest.py -c 1 -r 1 -s 2
+#python registerTest.py -c 1 -r 1 -s 3
+#python registerTest.py -c 1 -r 1 -s 4
 #RM2
-python registerTest.py -c 1 -r 2 -s 1
-python registerTest.py -c 1 -r 2 -s 2
-python registerTest.py -c 1 -r 2 -s 3
-python registerTest.py -c 1 -r 2 -s 4
+#python registerTest.py -c 1 -r 2 -s 1
+#python registerTest.py -c 1 -r 2 -s 2
+#python registerTest.py -c 1 -r 2 -s 3
+#python registerTest.py -c 1 -r 2 -s 4
 #RM3
-python registerTest.py -c 1 -r 3 -s 1
-python registerTest.py -c 1 -r 3 -s 2
-python registerTest.py -c 1 -r 3 -s 3
-python registerTest.py -c 1 -r 3 -s 4
+python registerTest.py -c 0 -S b -r 3 -s 1
+python registerTest.py -c 0 -S b -r 3 -s 2
+python registerTest.py -c 0 -S b -r 3 -s 3
+python registerTest.py -c 0 -S b -r 3 -s 4
 #RM4
-python registerTest.py -c 1 -r 4 -s 1
-python registerTest.py -c 1 -r 4 -s 2
-python registerTest.py -c 1 -r 4 -s 3
-python registerTest.py -c 1 -r 4 -s 4
+python registerTest.py -c 0 -S b -r 4 -s 1
+python registerTest.py -c 0 -S b -r 4 -s 2
+python registerTest.py -c 0 -S b -r 4 -s 3
+python registerTest.py -c 0 -S b -r 4 -s 4
 
 #rm *.tmp
